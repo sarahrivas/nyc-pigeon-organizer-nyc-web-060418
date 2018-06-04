6ec82d69dcd6bf228daacb81524dfeb1b64e8763
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-06-04 03:19:01 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  birds = {}
+  data.each do |category, types|
+    types.each do |type_key, names|
+      names.each do |name|
+        if birds[name] 
+          if birds[name][category]
+            birds[name][category] << type_key.to_s
+          else
+            birds[name][category] = [type_key.to_s] 
+          end  
+        else
+          birds[name] = {}
+          birds[name][category] = [type_key.to_s] 
+        end
+      end  
+    end
+  end
+  birds
 end
